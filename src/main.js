4073afc87226d84b816529cfd9c3bc85f3ea1f83
PastArizox <arizoxfr@gmail.com>
2025-07-23 22:05:29 +0200

    Added player movements

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Blob from './blob.js';
+import Player from './player.js';
 
 const canvas = document.getElementById('myCanvas');
 canvas.style.border = '1px solid black';
@@ -6,13 +7,43 @@ const canvasContext = canvas.getContext('2d');
 canvas.width = 900;
 canvas.height = 900;
 
-const player = new Blob(450, 450, 40, 'blue');
-player.draw(canvasContext);
-
-const blobs = Array.from({ length: 20 }, () => {
-    const blobRadius = 10;
+const blobs = Array.from({ length: 30 }, () => {
     const x = Math.random() * canvas.width;
     const y = Math.random() * canvas.height;
-    return new Blob(x, y, blobRadius, 'green');
+    return new Blob(x, y, 10, 'green');
 });
-blobs.forEach((blob) => blob.draw(canvasContext));
+
+const player = new Player(450, 450, 40, 'blue');
+var mouseX = 0;
+var mouseY = 0;
+
+canvas.addEventListener('mousemove', (event) => {
+    mouseX = event.offsetX;
+    mouseY = event.offsetY;
+});
+
+function update() {
+    canvasContext.save();
+
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+
+    centerPlayer();
+
+    blobs.forEach((blob) => {
+        blob.draw(canvasContext);
+    });
+
+    player.draw(canvasContext);
+    player.moveTo(mouseX, mouseY, canvas);
+
+    canvasContext.restore();
+}
+
+function centerPlayer() {
+    canvasContext.translate(
+        canvas.width / 2 - player.x,
+        canvas.height / 2 - player.y
+    );
+}
+
+setInterval(update, 1000 / 60);
---
+++ src/player.js
@@ -0,0 +1,19 @@
+import Blob from './blob.js';
+
+export default class Player extends Blob {
+    constructor(x, y, radius, color) {
+        super(x, y, radius, color);
+        this.velocity = 2;
+    }
+
+    moveTo(targetX, targetY, canvas) {
+        const dx = targetX - canvas.width / 2;
+        const dy = targetY - canvas.height / 2;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance > 0) {
+            this.x += (dx / distance) * this.velocity;
+            this.y += (dy / distance) * this.velocity;
+        }
+    }
+}
